e932f19346e5bf9786fbe804f71dfc9041c8a454
pranit-yawalkar <pranityawalkar10@gmail.com>
2022-07-25 07:44:20 +0530

    hystrix dashboard removed

--- extra-services/wishlist-service/src/main/java/com/wipro/shopforhome/wishlistservice/exception/AuthenticationFailException.java
+++
@@ -1,8 +0,0 @@
-package com.wipro.shopforhome.wishlistservice.exception;
-
-public class AuthenticationFailException extends IllegalArgumentException {
-    public AuthenticationFailException(String message) {
-        super(message);
-    }
-}
-
